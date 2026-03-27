aaddccf1f73eb9ccd1940dec799217cb5aef0510
Mogo Gu <shiroisenshiy@gmail.com>
2026-03-26 21:39:44 -0300

    Add files via upload

---
+++ exercício002.py
@@ -0,0 +1,8 @@
+n = input('Digite algo: ')
+print (type(n))
+print('É número?', (n).isnumeric())
+print('É letra?', (n).isalpha())
+print('É alfanúmerico?', (n).isalnum())
+print('Está em maiúsculo?', (n).isupper())
+print('Está em minúsculo?', (n).islower())
+print('É espaço?', (n).isspace())
