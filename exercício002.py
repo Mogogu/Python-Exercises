e2b8061a08e0f51eb73a122efaa417fba0bd2e60
Mogo Gu <shiroisenshiy@gmail.com>
2026-03-28 12:20:01 -0300

    exercício002.py

--- exercício002.py
+++ exercício002.py
@@ -1,4 +1,7 @@
-#Second exercise. To learn about the input
+# second exercise. To learn about the input
+# basically i'm using native commands to return 'false' or 'true' for characteristics of the input digit
+
+print('Considere que "false" é "falso", e "true" é "verdadeiro"...')
 n = input('Digite algo: ')
 print (type(n))
 print('É número?', (n).isnumeric())
