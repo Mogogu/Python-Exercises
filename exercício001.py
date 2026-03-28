2ecab92ee817e337f297399ebf1178a157a1e793
Mogo Gu <shiroisenshiy@gmail.com>
2026-03-28 12:15:31 -0300

    Update exercício001.py

--- exercício001.py
+++ exercício001.py
@@ -1,5 +1,6 @@
-#The first exercise of many.
-num1 = int(input ('Digite um número:'))
-num2 = int(input ('Digite outro número:'))
+# The first exercise of many
+
+num1 = int(input ('Digite um número: '))
+num2 = int(input ('Digite outro número: '))
 soma = num1 + num2
 print('A soma entre {} e {} vale {}'.format(num1, num2, soma))
