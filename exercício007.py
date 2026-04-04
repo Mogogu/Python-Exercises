538077839b0657d57aa7c677bde2872a67ddbd34
Mogo Gu <shiroisenshiy@gmail.com>
2026-04-03 23:32:32 -0300

    exercício007.py

---
+++ exercício007.py
@@ -0,0 +1,5 @@
+# code for showing the number for *0 until *10 (like input 3, it will show the multiples of 3 until 3*10, wich is equal 30)
+
+print('Para descobrir a tabuada de um número')
+n = int(input('Digite o número: '))
+print(n*0, n*1, n*2, n*3, n*4, n*5, n*6, n*7, n*8, n*9, n*10)
