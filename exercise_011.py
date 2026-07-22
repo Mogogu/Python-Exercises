a50d16114773d2bfd5647de839e8a392702adf8c
Guilherme Gomes <shiroisenshiy@gmail.com>
2026-07-22 10:29:45 -0300

    Create exercise_011.py

---
+++ exercise_011.py
@@ -0,0 +1,5 @@
+# conversion of real (BRL) to dolar (USD) where USD = 3.30BRL
+
+print('Para descobrir quantos dólares você compra com um valor em real, na cotação de 3,30 o dólar')
+nd1 = float(input('Digite o valor em real:'))
+print (nd1/3.30)
