cee4515dd208df0b8bd4c2579330c1ad55aa5535
Guilherme Gomes <shiroisenshiy@gmail.com>
2026-07-23 08:49:09 -0300

    Update exercise_007.py

--- exercise_007.py
+++ exercise_007.py
@@ -1,5 +1,12 @@
-# code for showing the number for *0 until *10 (like input 3, it will show the multiples of 3 until 3*10, wich is equal 30)
+# to know the multiplication table of a number
 
 print('Para descobrir a tabuada de um número')
-n = int(input('Digite o número: '))
-print(n*0, n*1, n*2, n*3, n*4, n*5, n*6, n*7, n*8, n*9, n*10)
+nc1 = int(input('Digite o número:'))
+print(nc1*0, nc1*1, nc1*2, nc1*3, nc1*4, nc1*5, nc1*6, nc1*7, nc1*8, nc1*9, nc1*10)
+
+# the newest version use for to show the multiplication in a better format
+
+print('Para saber a tabuada de 1x a 10x...')
+d1 = int(input('Escreva o número que quer saber a tabuada: '))
+for d2 in range(1, 11):
+    print('| {} x {} | = {} '.format(d1, d2, d1*d2))
