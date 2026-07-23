1669628d3212a35cb4d21f708f308936fc59e1fc
Guilherme Gomes <shiroisenshiy@gmail.com>
2026-07-23 08:54:26 -0300

    Update exercise_010.py

--- exercise_010.py
+++ exercise_010.py
@@ -1,12 +1,7 @@
-# to know the multiplication table of a number
+# an exercise that shows the value of an item after 5% discount
 
-print('Para descobrir a tabuada de um número')
-nc1 = int(input('Digite o número:'))
-print(nc1*0, nc1*1, nc1*2, nc1*3, nc1*4, nc1*5, nc1*6, nc1*7, nc1*8, nc1*9, nc1*10)
-
-# the newest version use for to show the multiplication in a better format
-
-print('Para saber a tabuada de 1x a 10x...')
-d1 = int(input('Escreva o número que quer saber a tabuada: '))
-for d2 in range(1, 11):
-    print('| {} x {} | = {} '.format(d1, d2, d1*d2))
+print('Para calcular um desconto de 5% sobre o valor de um item/objetivo:')
+nf1 = float(input('Qual o preço do item?'))
+nf2 = (nf1/100) * 5
+nf3 = nf1-nf2
+print('Com o desconto de 5%, seu item de {} sai à {:.2f}'.format(nf1, nf3))
